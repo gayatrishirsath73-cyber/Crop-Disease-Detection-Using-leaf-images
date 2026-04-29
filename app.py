aae9eb2b5f223f733439f9b7eb8fbee9f3b464bb
Gayatri Shirsath <gayatrishirsath73@gmail.com>
2026-04-30 02:49:59 +0530

    fix model path error

--- app.py
+++ app.py
@@ -1,37 +1,14 @@
-import os
-model_path = os.path.join(os.path.dirname(__file__), "model.keras")
-model = tf.keras.models.load_model(model_path, compile=False)
-
 import streamlit as st
 import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-import keras
-model = keras.models.load_model("model.h5", compile=False)
+# FIX: define model path correctly
+model_path = "model.h5"
+
+model = tf.keras.models.load_model(model_path, compile=False)
+
 classes = [
 "Pepper__bell___Bacterial_spot",
 "Pepper__bell___healthy"
 ]
-
-st.title("Crop Disease Detection")
-
-uploaded_file=st.file_uploader(
-"Upload Leaf Image",
-type=["jpg","jpeg","png"]
-)
-
-if uploaded_file is not None:
-
-    image=Image.open(uploaded_file).convert("RGB")
-    st.image(image,width=300)
-
-    img=image.resize((128,128))
-    img=np.array(img)/255.0
-    img=np.expand_dims(img,axis=0)
-
-    pred=model.predict(img)
-
-    result=classes[np.argmax(pred)]
-
-    st.success(result)
