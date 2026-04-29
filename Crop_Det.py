3dea3bc2d9bbb52517ace7678d7c8d0561f0435d
Gayatri Shirsath <gayatrishirsath73@gmail.com>
2026-04-30 03:15:48 +0530

    final crop disease project

--- Crop_Det.py
+++ Crop_Det.py
@@ -1,67 +1,66 @@
 import tensorflow as tf
-from tensorflow.keras import layers,models
-import matplotlib.pyplot as plt
+from tensorflow.keras import layers, models
 
-data_dir="Dataset PlantVillage"
+data_dir = "Dataset PlantVillage"
 
-img_height=128
-img_width=128
-batch_size=32
+img_height = 128
+img_width = 128
+batch_size = 32
 
-train_ds=tf.keras.utils.image_dataset_from_directory(
-data_dir,
-validation_split=0.2,
-subset="training",
-seed=123,
-image_size=(img_height,img_width),
-batch_size=batch_size
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size
 )
 
-val_ds=tf.keras.utils.image_dataset_from_directory(
-data_dir,
-validation_split=0.2,
-subset="validation",
-seed=123,
-image_size=(img_height,img_width),
-batch_size=batch_size
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size
 )
 
-class_names=train_ds.class_names
-print(class_names)
+class_names = train_ds.class_names
+print("Classes:", class_names)
 
-AUTOTUNE=tf.data.AUTOTUNE
-train_ds=train_ds.cache().shuffle(1000).prefetch(AUTOTUNE)
-val_ds=val_ds.cache().prefetch(AUTOTUNE)
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.cache().shuffle(1000).prefetch(AUTOTUNE)
+val_ds = val_ds.cache().prefetch(AUTOTUNE)
 
-model=models.Sequential([
-layers.Rescaling(1./255,input_shape=(128,128,3)),
+model = models.Sequential([
+    layers.Rescaling(1./255, input_shape=(128, 128, 3)),
 
-layers.Conv2D(32,3,activation='relu'),
-layers.MaxPooling2D(),
+    layers.Conv2D(32, 3, activation='relu'),
+    layers.MaxPooling2D(),
 
-layers.Conv2D(64,3,activation='relu'),
-layers.MaxPooling2D(),
+    layers.Conv2D(64, 3, activation='relu'),
+    layers.MaxPooling2D(),
 
-layers.Conv2D(128,3,activation='relu'),
-layers.MaxPooling2D(),
+    layers.Conv2D(128, 3, activation='relu'),
+    layers.MaxPooling2D(),
 
-layers.Flatten(),
-layers.Dense(128,activation='relu'),
-layers.Dense(2,activation='softmax')
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(2, activation='softmax')
 ])
 
 model.compile(
-optimizer='adam',
-loss='sparse_categorical_crossentropy',
-metrics=['accuracy']
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
 )
 
-history=model.fit(
-train_ds,
-validation_data=val_ds,
-epochs=10
+history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=10
 )
 
-model.save("model.keras")
+model.save("model.h5")
 
 print("Model Saved Successfully")
